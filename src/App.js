55c55c4aff56afa5d6a562a83971d53d9f96522c
Sebastian Jimenez <sjimnezc@cesde.net>
2024-05-18 16:46:54 -0500

    landing sin estilo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { LandingPage } from './components/landingPage/LandingPage';
+import { Login } from './components/login/Login';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h1>Editado por Santiago Hernandez</h1>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Login/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
